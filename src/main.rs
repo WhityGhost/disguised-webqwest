7a125a1f4f2d9214b8a459d99159a12bf9e9a2e8
Hamilton Munic <itswhityminato@gmail.com>
2024-07-04 18:31:52 +0200

    init: sample for tcl&wss

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use reqwest_impersonate as reqwest;
+use std::error::Error;
+
+use futures_util::{SinkExt, StreamExt, TryStreamExt};
+use reqwest::{impersonate::Impersonate, Client, Message};
+
+// tcp url: https://answerthepublic.com
+// websocket url: wss://answerthepublic.com/
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Build a client to mimic Chrome123
+    let client = reqwest::Client::builder()
+        .impersonate(Impersonate::Chrome126)
+        .enable_ech_grease()
+        .permute_extensions()
+        .build()?;
+
+    // Use the API you're already familiar with
+    let resp = client.get("https://answerthepublic.com").send().await?;
+    println!("{}", resp.text().await?);
+
+    let websocket = Client::builder()
+        .impersonate_websocket(Impersonate::Chrome126)
+        .build()?
+        .get("wss://echo.websocket.org")
+        .upgrade()
+        .send()
+        .await?
+        .into_websocket()
+        .await?;
+
+    let (mut tx, mut rx) = websocket.split();
+
+    tokio::spawn(async move {
+        for i in 1..11 {
+            tx.send(Message::Text(format!("Hello, World! #{i}")))
+                .await
+                .unwrap();
+        }
+    });
+
+    while let Some(message) = rx.try_next().await? {
+        match message {
+            Message::Text(text) => println!("received: {text}"),
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
